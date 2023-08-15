07e4fcf1cad1b89a39084d7b2f1e05cdb3e84519
juansesu3 <juanseu3@hotmail.com>
2023-08-15 20:43:09 +0200

    FEATURE(IMAGES) now I know image/next working

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image"
 
 const Header = () => {
   return (
     <header>
-      Header
+      <Image 
+      src={"https://links.papareact.com/c2cdd5"} 
+      alt="Trello logo"
+      width={300}
+      height={100}
+      className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+      />
     </header>
   )
 }
