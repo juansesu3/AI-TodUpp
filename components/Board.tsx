12ad9d973a58c78940bfcb01f4489f4434290077
juansesu3 <juanseu3@hotmail.com>
2023-08-16 10:29:21 +0200

    FEATURE(Typings MapObject)

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,13 @@
 "use client"
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+
+  useEffect(()=>{
+    // getboard();
+
+  },[])
   return (
     <DragDropContext>
       <Droppable droppableId="board" direction="horizontal" type="column">
