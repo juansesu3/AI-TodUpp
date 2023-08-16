36ea0999d5fc0b54c7ac07b213e20828642f1a80
juansesu3 <juanseu3@hotmail.com>
2023-08-16 14:25:46 +0200

    FEATURE(TodoCard)and I'm going to implement the logic to card and columns save de move

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,43 +5,48 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 const Board = () => {
-  const [board, getBoard] = useBoardStore((state)=> [
+  const [board, getBoard] = useBoardStore((state) => [
     state.board,
     state.getBoard
   ]);
 
-  useEffect(()=>{
-     getBoard();
+  useEffect(() => {
+    getBoard();
 
-  },[getBoard])
-  
-const handleDragEnd =(result: DropResult)=>{
+  }, [getBoard])
+
+  const handleDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    console.log(destination);
+    console.log(source);
+    console.log(type);
 
 
-}
+  }
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => 
-        <div
-        className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
-        {...provided.droppableProps}
-        ref={provided.innerRef}
-        >{
-          Array.from(board.columns.entries()).map(([id, column], index)=>(
-            <Column 
-              key={id}
-              id={id}
-              todos={column.todos}
-              index={index} 
-            />
-          ))
-          }</div>
+        {(provided) =>
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >{
+              Array.from(board.columns.entries()).map(([id, column], index) => (
+                <Column
+                  key={id}
+                  id={id}
+                  todos={column.todos}
+                  index={index}
+                />
+              ))
+            }</div>
         }
       </Droppable>
-  </DragDropContext> 
-   
+    </DragDropContext>
+
   );
 };
 
---
+++ components/TodoCard.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { XCircleIcon } from "@heroicons/react/20/solid";
+import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from "react-beautiful-dnd";
+
+type Props = {
+    todo: Todo;
+    index: number;
+    id: TypedColumn;
+    innerRef: (element: HTMLElement | null) => void;
+    draggleProps: DraggableProvidedDraggableProps;
+    draggleHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+}
+
+const TodoCard = ({
+    todo,
+    index,
+    id,
+    innerRef,
+    draggleProps,
+    draggleHandleProps,
+
+}: Props) => {
+    return (
+        <div
+            className="bg-white rounded-md space-y-2 mt-2 drop-shadow-md"
+            {...draggleProps}
+            {...draggleHandleProps}
+            ref={innerRef}>
+            <div className="flex justify-between items-center p-5">
+                <p>
+                    {todo.title}
+                </p>
+                <button className="text-red-500 hover:text-red-600">
+                    <XCircleIcon className="ml-5 h-8 w-8" />
+                </button>
+            </div>
+            {/*Add image here...  */}
+        </div>
+    )
+}
+
+export default TodoCard
