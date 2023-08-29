13bc6aa9a478fb27c5c492d6a648d4eed38c9dca
juansesu3 <juanseu3@hotmail.com>
2023-08-29 21:32:39 +0200

    FIX(UI)

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -2,11 +2,18 @@ import { ID, storage } from "@/appwrite";
 
 const uploadImage = async (file: File) => {
   if (!file) return;
+
+  const bucketId = process.env.BUCKET_ID;
+  if (!bucketId) {
+    throw new Error("Bucket ID is not defined in the environment variables.");
+  }
+
   const fileUploaded = await storage.createFile(
-    process.env.BUCKET_ID,
+    bucketId,
     ID.unique(),
     file
   );
   return fileUploaded;
 };
+
 export default uploadImage;
